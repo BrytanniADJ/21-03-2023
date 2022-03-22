d658cd424f39eea31a0501df01fb18a2a2bb9855
Prof. Wisley ALves <wis.tech7l@gmail.com>
2022-03-21 21:59:47 -0300

    teste

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,6 +6,7 @@ const sexo = 'M'
 // imprimir no console
 console.log(nome)
 
+// comandos de condição
 const comparaIdade = function (idade){
     let idadeMax = 30
     let op = 3
@@ -31,17 +32,56 @@ const comparaIdade = function (idade){
     }
 
 }
-
+// comando de repetição
 const repeticao = function () {
     for(let i = 0; i < 10; i++){
         console.log('> ', i)
     }
 }
 
+// ARRAY
+const arrays = function () {
+    let idade = [12,30,50,45,28,24,70,80,15]
+    console.log(idade.length)
 
+    for(let i = 0; i < idade.length; i++){
+        console.log('> ', idade[i])
+    }
+    
+}
+
+// objeto
+const pessoas = function(){
+    let arrayPessoas = []
+
+    let nome = 'Wisley Alves'
+    let idade = 28
+    let sexo = 'M'
+
+    let pessoa = {
+        nome,
+        idade,
+        sexo
+    }
+
+    arrayPessoas.push(pessoa)
+
+    nome = 'Claúdia Aguilar'
+    sexo = 'F'
+    
+    pessoa = {
+        nome,
+        idade,
+        sexo
+    }
+
+    arrayPessoas.push(pessoa)
+
+    console.log(arrayPessoas)
+}
 
 const inicio = function () {
-    repeticao()    
+    pessoas()
 }
 
 inicio()
